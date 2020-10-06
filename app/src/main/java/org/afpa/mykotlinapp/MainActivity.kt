205bf883eb618ea8fc6d1aab369c78b15bbe6e37
Germain SIPIERE <gsipiere@gmail.com>
2020-10-06 19:57:51 +0200

    add visuel and remove actionbar

--- app/src/main/java/org/afpa/mykotlinapp/MainActivity.kt
+++ app/src/main/java/org/afpa/mykotlinapp/MainActivity.kt
@@ -6,39 +6,38 @@ import android.os.Bundle
 import android.text.TextUtils
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var diceImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = roll_button
-        val textToChange: TextView = text_to_change
-        val addOne: Button = plus_un
-        addOne.setOnClickListener { augmente() }
+        diceImage = dice_image
+
         rollButton.setOnClickListener { rollDice() }
 
     }
 
-    private fun augmente() {
-
-        var lastRoll = text_to_change.text.toString().toIntOrNull()
-
-        if (lastRoll != null && lastRoll < 6) text_to_change.text = (lastRoll + 1).toString()
-
-    }
 
     private fun rollDice() {
-        Toast.makeText(this, "bouton cliqué", Toast.LENGTH_SHORT).show()
-        text_to_change.setTextColor(Color.MAGENTA)
-        //text_to_change.text = "Dice Rolled"
-        val randomInt = (1..6).random()
-        text_to_change.text = randomInt.toString()
 
+        val drawableResource = when ((1..6).random()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
     }
 
 
